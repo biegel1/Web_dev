093200a89c2bc5df8021655814967a723cb3005e
Tobias Biegel <tobias.biegel@tu-darmstadt.de>
2021-07-13 20:38:03 +0200

    adds employee function component

--- projects/website_one/biegel/src/index.js
+++ projects/website_one/biegel/src/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+function Employees(){
+  const listOfEmployees = [{
+    name: 'Max Mustermann',
+    description: 'Max Mustermann Junior ist der Geschäftsführer des Unternehmens und ist zuständig für die Betriebsleitung, Vertrieb und vieles mehr'
+  },
+  {
+    name: 'Max Mustermann', 
+    description: 'Max Mustermann ist die Geschäftsführerin des Unternehmens und seit über 50 Jahre für das Controlling zuständig'
+  }
+];
+  return (
+    <div>
+      {listOfEmployees.map((employee) => {
+        return ( 
+          <RenderEmployee key = {employee.toString()} employee = {employee}/>
+        )
+      })}
+    </div>
+  );
+}
+
+function RenderEmployee(props){
+    const name = props.employee.name;
+    const description = props.employee.description;
+    return (
+      <div class = 'flex space-x-4'>
+        <button class = 'mb-8'>{name}</button>
+        <button class = 'mb-8'>{description}</button>
+      </div>
+    );
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Employees/>
+  </div>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
