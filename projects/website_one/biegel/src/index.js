c906b8b2cfdeb853c6aab585a7f76281583ef32d
Tobias Biegel <tobias.biegel@tu-darmstadt.de>
2021-07-14 20:39:25 +0200

    adds further styling to employee function component

--- projects/website_one/biegel/src/index.js
+++ projects/website_one/biegel/src/index.js
@@ -2,24 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import logo from './images/biegel_tobias.jpg';
 
 
 
 function Employees(){
   const listOfEmployees = [{
     name: 'Max Mustermann',
-    description: 'Max Mustermann Junior ist der Geschäftsführer des Unternehmens und ist zuständig für die Betriebsleitung, Vertrieb und vieles mehr'
+    description: 'Max Mustermann Junior ist der Geschäftsführer des Unternehmens und ist zuständig für die Betriebsleitung, Vertrieb und vieles mehr',
+    position: 'CEO',
+    picture: logo,
   },
   {
     name: 'Max Mustermann', 
-    description: 'Max Mustermann ist die Geschäftsführerin des Unternehmens und seit über 50 Jahre für das Controlling zuständig'
+    description: 'Max Mustermann ist die Geschäftsführerin des Unternehmens und seit über 50 Jahre für das Controlling zuständig',
+    position:'R&D',
+    picture:logo,
   }
 ];
   return (
     <div>
-      {listOfEmployees.map((employee) => {
+      {listOfEmployees.map((employee, i) => {
         return ( 
-          <RenderEmployee key = {employee.toString()} employee = {employee}/>
+          <RenderEmployee key = {i.toString()} employee = {employee}/>
         )
       })}
     </div>
@@ -29,10 +34,26 @@ function Employees(){
 function RenderEmployee(props){
     const name = props.employee.name;
     const description = props.employee.description;
+    const position = props.employee.position;
+    const picture = props.employee.picture;
     return (
-      <div class = 'flex space-x-4'>
-        <button class = 'mb-8'>{name}</button>
-        <button class = 'mb-8'>{description}</button>
+      <div className = 'pt-6 shadow-2xl w-auto'>
+        <figure className =  'flex bg-gray-100 p-8'>
+          <img className = 'w-32 h-30' src = {picture} alt = 'Tobias Biegel' />
+          <div className = 'pt-6 pl-4 text-left space-y-4'>
+            <p className = 'text-lg font-semibold'>
+              {description}
+            </p>
+            <figcaption className = 'font-medium'>
+              <div className = 'text-cyan-600'>
+                {name}
+              </div>
+              <div className = 'text-gray-500'>
+                {position}
+              </div>
+            </figcaption>
+          </div>
+        </figure>
       </div>
     );
 }
